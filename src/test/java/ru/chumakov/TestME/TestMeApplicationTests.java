3830a0a3e7445361ac99f724799190a25faa9b90
Alexei Chumakov <chumakovalexei@mail.ru>
2020-12-22 13:06:29 +0300

    Initial commit

---
+++ src/test/java/ru/chumakov/TestME/TestMeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.chumakov.TestME;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
